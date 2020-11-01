4e7909f4ef97571fcf460032166a5d158cc62be4
xiaowenlg <daiyu0@163.com>
2020-11-01 15:30:34 +0800

    加入线程

--- 工程生成版本/Src/freertos.c
+++ 工程生成版本/Src/freertos.c
@@ -1,4 +1,4 @@
-﻿/* USER CODE BEGIN Header */
+/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * File Name          : freertos.c
@@ -23,11 +23,10 @@
 #include "task.h"
 #include "main.h"
 #include "cmsis_os.h"
-#include "WTN6.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */     
-#include "button.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,27 +46,13 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-uint8_t  TIM5CH1_CAPTURE_STA = 0;	//ÊäÈë²¶»ñ×´Ì¬		    				
-uint16_t	TIM5CH1_CAPTURE_VAL;	//ÊäÈë²¶»ñÖµ
-uint32_t temp = 0;
-//按键变量
-uint16_t KeyValue_t = 0xffff; uint16_t lastvalue_t = 0xffff;
-Key_Message keys[2] = { 0 };
-uint8_t flag = 0;
-float res;
 
-//线程句柄
-osThreadId SensorDriveHandle;//传感器驱动线程
-osThreadId ListenerTaskHandle;    //监听线程
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-//按键注册和事件回调函数
-void  Key_CallBack(Key_Message index);
-void SensorDrive_CallBack(void const *argument);
-void Listener_CallBack(void const *argument);
+   
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void const * argument);
@@ -138,85 +123,19 @@ void MX_FREERTOS_Init(void) {
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void const * argument)
 {
+
   /* USER CODE BEGIN StartDefaultTask */
-	
-	taskENTER_CRITICAL(); //进入临界区
-
-	osThreadDef(SensorDrive, SensorDrive_CallBack, 4, 0, 128);
-	SensorDriveHandle = osThreadCreate(osThread(SensorDrive), NULL);
-
-	osThreadDef(Listener,Listener_CallBack,2,0,128);
-	ListenerTaskHandle = osThreadCreate(osThread(Listener), NULL);
-
-	vTaskDelete(defaultTaskHandle); //删除任务
-	taskEXIT_CRITICAL();//推出临界区
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
   /* USER CODE END StartDefaultTask */
 }
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
-void SensorDrive_CallBack(void const *argument)
-{
-	for (;;)
-	{
-		ScanKeys(&KeyValue_t, &lastvalue_t, keys, Key_CallBack);
-		//printf1("in while \r\n");
-		osDelay(20);
-	}
-}
-void Listener_CallBack(void const *argument)
-{
-	for (;;)
-	{
-		//printf1("task listener************************** \r\n");
-		osDelay(500);
-	}
-}
-void  Key_regist(void)
-{
-	keys[0].GPIOx = GPIOA;
-	keys[0].GPIO_Pin = KEY_START_Pin;
-	keys[0].Key_count = 2;
-
-	keys[1].GPIOx = GPIOA;
-	keys[1].GPIO_Pin = KEY_USER_Pin;
-	keys[1].Key_count = 2;
-}
-//按键回调函数
-void  Key_CallBack(Key_Message index)
-{
-	//printf1("The KEY_USER is passed!\r\n");
-	if (index.GPIO_Pin == KEY_START_Pin)
-	{
-		printf1("The KEY_START is passed_pin8!\r\n");
-	}
-	if (index.GPIO_Pin == KEY_USER_Pin)
-	{
-		printf1("The KEY_USER is passed_pin11!\r\n");
-		flag = 1;
-		TIM5CH1_CAPTURE_STA = 0;
-		while (flag)
-		{
-			
-			printf1("in while \r\n");
-			osDelay(10);
-			if (TIM5CH1_CAPTURE_STA & 0X80)
-			{
-				temp = TIM5CH1_CAPTURE_STA & 0X3F;
-				temp *= 65536;//溢出时间总和
-				temp += TIM5CH1_CAPTURE_VAL;////得到总的高电平时间
-				printf("HIGH:%d us\r\n", temp);//打印总的高点平时间
-				TIM5CH1_CAPTURE_STA = 0;//开始下一次捕获
-				res = temp / 1000.00 / 1000.00;
-				//res = (res*10);
-				printf("HIGH/1000====%dmm\r\n", (uint32_t)(res*res*1250));////输出毫米
-				flag = 0;
-			}
-			
-		}
-		
-	}
-}
+     
 /* USER CODE END Application */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
